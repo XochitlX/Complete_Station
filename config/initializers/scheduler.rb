bb27da3cd505a40e30f5f94730359f6027371e46
XochitlX <xochitl.xtm@gmail.com>
2017-08-25 20:08:19 -0500

    to make change minutes

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -1,18 +1,18 @@
 
 require 'rufus-scheduler'
 
-s = Rufus::Scheduler.singleton
+# s = Rufus::Scheduler.singleton
 
 
 
-	#s.every '1m' do
-	#s.at '2017/08/25 14:50:00' do
-	s.every '5m' do
- 		Rails.logger.info "hello, it's #{Time.now}"
-		Event.reminder
+# 	#s.every '1m' do
+# 	#s.at '2017/08/25 14:50:00' do
+# 	s.every '5m' do
+#  		Rails.logger.info "hello, it's #{Time.now}"
+# 		Event.reminder
  		
 
-	end
+# 	end
 
 
 
